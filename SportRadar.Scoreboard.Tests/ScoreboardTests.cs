1bf67bad1bc8dbf4f42fef96c7183ba25ce9b815
Adel Pourramezan Ganji <adelganji@gmail.com>
2023-03-15 12:26:11 +0330

    We Add Team and Match Entities and MatchStatus Enum to project and create a team an create a match between two teams

---
+++ SportRadar.Scoreboard.Tests/ScoreboardTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportRadar.Scoreboard.Domain.Entities;
+using SportRadar.Scoreboard.Domain.Enums;
+
+namespace SportRadar.Scoreboard.Tests;
+
+public class ScoreboardTests
+{
+    public class TeamTests
+    {
+        [Theory]
+        [InlineData("Mexico")]
+        [InlineData("Canada")]
+        [InlineData("Spain")]
+        [InlineData("Brazil")]
+        [InlineData("Germany")]
+        [InlineData("France")]
+        [InlineData("Uruguay")]
+        [InlineData("Italy")]
+        [InlineData("Argentina")]
+        [InlineData("Australia")]
+        public void CreateTeam(string teamName)
+        {
+            var team = new Team(teamName);
+            Assert.Equal(team.TeamName, teamName);
+        }
+    }
+
+    public class MatchTests
+    {
+        [Theory]
+        [InlineData("Mexico", "Canada")]
+        [InlineData("Spain", "Brazil")]
+        [InlineData("Germany", "France ")]
+        [InlineData("Uruguay", "Italy ")]
+        [InlineData("Argentina", "Australia")]
+        public void CreateAMatchBetweenTwoTeams(string homeTeam, string awayTeam, MatchStatus matchStatusExpectedValue = MatchStatus.NOT_STARTED)
+        {
+            if(homeTeam == awayTeam)
+                throw new ArgumentException("Home team name and away team name cannot be the same.", nameof(awayTeam));
+            var _homeTeam = new Team(homeTeam);
+            var _awayTeam = new Team(awayTeam);
+
+            var _newMatch = new Match(_homeTeam, _awayTeam);
+
+            Assert.Equal(_newMatch.HomeTeam.TeamName, homeTeam);
+            Assert.Equal(_newMatch.AwayTeam.TeamName, awayTeam);
+
+            Assert.Equal(_newMatch.Status, matchStatusExpectedValue);
+        }
+
+    }
+
+}
---
+++ SportRadar.Scoreboard/Domain/Entities/Match.cs
@@ -0,0 +1,33 @@
+﻿using SportRadar.Scoreboard.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportRadar.Scoreboard.Domain.Entities;
+
+public class Match
+{
+    public Team HomeTeam { get; private set; }
+    public Team AwayTeam { get; private set; }
+    public MatchStatus Status { get; private set; }
+
+    /// <summary>
+    /// Create a footbal match with 2 footbal teams.
+    /// </summary>
+    /// <param name="homeTeam">The one who is hosting the game.</param>
+    /// <param name="awayTeam">The team that is visiting and playing at their opponent's home field.</param>
+    public Match(Team homeTeam, Team awayTeam)
+    {
+        if (homeTeam == null || awayTeam == null)
+            throw new InvalidOperationException("'HomeTeam' and 'AwayTeam' must be initialized correctly.");
+        if (homeTeam.TeamName == awayTeam.TeamName)
+            throw new ArgumentException("Home team name and away team name cannot be the same.", nameof(awayTeam));
+
+        HomeTeam = homeTeam;
+        AwayTeam = awayTeam;
+        Status = MatchStatus.NOT_STARTED;
+    }
+
+}
